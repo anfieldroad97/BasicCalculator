ea95fc4013ee33b51e722deccdeb790f3c0d546a
Mirzhan <miko_555@icloud.com>
2025-09-17 22:19:23 +0500

    feat: add project

---
+++ BasicCalculator/BasicCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicCalculatorApp.swift
+//  BasicCalculator
+//
+//  Created by Mirzhan Gumarov on 17.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct BasicCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
